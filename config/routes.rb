02267ae93d3f72ef127a15308ba2a39fd9335ece
Kate <ms.katesy@gmail.com>
2018-08-14 14:07:49 +1000

    add root, created home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :games_users
   resources :games
   resources :users
+
+  get '/', to: 'pages#home'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
