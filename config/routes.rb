99874c4c60b64572d8a5dd4b7517d22155173dfe
Kate <ms.katesy@gmail.com>
2018-08-14 17:32:49 +1000

    added logout btn and function to games_new page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
 
   post '/sessions', to: 'sessions#create'
-
+  delete '/sessions', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
